f6467877492ef845d33cf5b5b20d9c5fb589568e
Sally Fung <sallyfung2018@gmail.com>
2024-03-15 23:27:29 +0800

    first commit

---
+++ src/main/java/com/sallyfunghk/mywebbackend/PersonalWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sallyfunghk.mywebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sallyfunghk/mywebbackend/PersonalWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sallyfunghk.mywebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
