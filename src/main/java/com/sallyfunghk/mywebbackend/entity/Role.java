919a393eb0a06e40132a2ae5c5992740dfea065f
Sally Fung <sallyfung2018@gmail.com>
2024-03-17 13:33:17 +0800

    create User and Role entity

---
+++ src/main/java/com/sallyfunghk/mywebbackend/entity/Role.java
@@ -0,0 +1,18 @@
+package com.sallyfunghk.mywebbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+}
---
+++ src/main/java/com/sallyfunghk/mywebbackend/entity/User.java
@@ -0,0 +1,34 @@
+package com.sallyfunghk.mywebbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "is_active")
+    private boolean is_active;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
